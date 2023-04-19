d306b53ace582cf35dac429bb54c7fad88ac8823
Sainty93 <jenandfloyd93@icloud.com>
2023-04-19 14:21:22 -0700

    make corrections to contact me

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,61 +1,71 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../utils/helpers';
+import React, { useState, useEffect } from 'react';
 
 const ContactMe = () => {
-    const [nameInput, setNameInput] = useState('');
-    const [emailInput, setEmailInput] = useState('');
-    const [messageInput, setMessageInput] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+   const formInputValues = { name: '', email: '', message: '' };
+   const [formValues, setFormValues] = useState(formInputValues);
+   const [formErrors, setFormErrors] = useState({});
+   const [formSubmit, setFormSubmit] = useState(false);
 
     const handleInputChange = (e) => {
     const { name, value } = e.target;
 
-
-    name === 'name'
-    ? setNameInput(value)
-    : name === 'email'
-    ? setEmailInput(value)
-    : setMessageInput(value);
+    setFormValues({...formValues, [name]: value});
 };
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+        setFormErrors(validate(formValues));
+        setFormSubmit(true);
+    };
 
-    if (!validateEmail(emailInput)) {
-        setErrorMessage('Please enter a valid email');
-        return;
-    }
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && formSubmit) {
+            console.log(formValues);
+        }
+    }, [formErrors]);
 
-            setNameInput('');
-            setEmailInput('');
-            setMessageInput('');
+    const validate = (values) => {
+        const errors = {};
+        const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.name) {
+            errors.name = 'Please enter a name';
+        }
+        if (!values.email) {
+            errors.email = 'Please enter an email address';
+        }
+        if (!values.message) {
+            errors.message = 'Please enter a message';
+        }
+        return errors;
 };
    return (
-    <form className='w-auto my-10 md:w-[75vh] md:mx-auto'>
+    <div>
+    <form onSubmit={handleFormDSubmit} className='w-auto my-10 mx-10 md:w-[75vh] md:mx-auto'>
         <div className='mb-6'>
             <label htmlfor='name' className='block m-2 text-lg font-medium text-gray-900 dark:text-white'>Name</label>
-             <input value={nameInput} type='text' name='name' onChange={handleInputChange} className='bg-gray-50 border border-gray-300 text-gray-900
+             <input type='text' name='name' placeholder='Enter name here' value={formValues.name} onChange={handleInputChange} className='bg-gray-50 border border-gray-300 text-gray-900
              text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-             placeholder="Enter Name Here" required/>
+             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
         </div>
+        <p className='text-md text-red-700'>{formErrors.name}</p>
+
         <div className='mb-6'>
             <label htmlfor='email' className='block m-2 text-lg font-medium
              text-gray-900 dark:text-white'>Email</label>
-             <input value={emailInput} type='email' id='email' onChange={handleInputChange} className='bg-gray-50 border border-gray-300 text-gray-900
+             <input type='email' name='email' placeholder='Enter email here' value={formValues.email} onChange={handleInputChange} className='bg-gray-50 border border-gray-300 text-gray-900
              text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
              dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-             dark:focus:border-blue-500' placeholder='Enter email here' required/>
+             dark:focus:border-blue-500'/>
         </div>
+        <p className='text-md text-red-700'>{formErrors.email}</p>
             <div className='flex-items-start mb-6'>
                 <div className='flex-items-center h-5'></div>
             </div>
 
-            <label htmlfor='message' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your Message</label>    
-                <textarea value={messageInput} name='message' onChnage={handleInputChange} rows='4' className='block p-2.5 w-full text-sm text-gray-900 bg--gray-50
-                rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 
-                dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                 dark:focus:border-blue-500' placeholder='Write your thoughts here'> </textarea>
+            <label htmlFor='message' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Your Message</label>
+                <textarea type='text' name='message' rows='4' placeholder='Write your thoughts here' value={formValues.message} onChange={handleInputChange} className='block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'></textarea>
+                    <p className='text-md text-red-700'>{formErrors.message}</p>
                  
                  <button type='submit' onClick={handleFormSubmit} className='text-white mt-8 bg-blue-700 hover:bg-blue-800
                  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center
@@ -63,6 +73,7 @@ const ContactMe = () => {
             
             
             </form>
+        </div>
    );
 };
 
